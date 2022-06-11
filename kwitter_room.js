d8c3dc4276c79f80792d97c8a3c0943209b31587
Ariana <85405169+ItzMe456@users.noreply.github.com>
2022-06-11 08:55:57 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,27 @@
+
+const firebaseConfig = {
+      apiKey: "AIzaSyDtL65-gbAeguEUKaQ5rJzYRhK7tlbaKm8",
+      authDomain: "kwitter-a3159.firebaseapp.com",
+      projectId: "kwitter-a3159",
+      storageBucket: "kwitter-a3159.appspot.com",
+      messagingSenderId: "831414631641",
+      appId: "1:831414631641:web:5db557aac0c85a9bc212c8"
+    };
+    
+    const app = initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("user_name");
+      window.location = "kwitter.html";
+}
